822e4e6fc292f6d64addc7c78407cdfe6587349b
ElenaR1 <elena.radoycheva@abv.bg>
2017-01-31 10:23:56 +0200

    Update 2Kontrolno.hs

--- 2Kontrolno.hs
+++ 2Kontrolno.hs
@@ -1,3 +1,18 @@
+--[1,2,3,4,5]->(1+2):(3+4):5:[]=[3,7,5]
+sum2::[Int]->[Int]
+sum2 []=[]
+sum2 [x]=[x]
+sum2 (x:y:xs)=(x+y):sum2 xs
+
+--inward ["abcd","efgh","ijkl"]->"abcdlkjiefgh"
+--["ab","cd","ef","gh"]="abhgcdfe"
+inward::[String]->String
+inward []=""
+inward [x]=x
+inward (x:xs)=x++ reverse (last xs) ++inward (init xs)
+
+
+
 --3
 f::Int->Int
 f a=a*2
@@ -25,3 +40,5 @@ func3 a=map(\f-> f a)
 
 asdf :: a -> [a -> b] -> [b]
 asdf = (zipWith (flip ($))) . repeat
+
+--poslednata zadacha e z sortirane
