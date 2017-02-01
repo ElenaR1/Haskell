2d2bdc4f77598ece53b5453e55dee8c8913ef119
ElenaR1 <elena.radoycheva@abv.bg>
2017-02-01 12:34:02 +0200

    Update Scheme 3 4.scm

--- Scheme 3 4.scm
+++ Scheme 3 4.scm
@@ -348,3 +348,34 @@ PRAVILNO POLKZVANE NA MAP
 	        (* (expt (- 1) k) (/ (expt x (+ (* 2 k) 1)) (fact (+ (* 2 k) 1)))))
 		  (acc term3 + 0 0 next k))
 		 (acc term2 * 1 1 next p))
+
+
+;1/a+1/(a+1)+...+1/b
+(define (term a) (/ 1 a))
+(define (next a) (+ a 1))
+(define (accumulate op term null-value a next b)
+    (if (> a b)
+        null-value
+        (op (term a) (accumulate op term null-value (next a) next b))))
+(define (f a b) (accumulate + term 0 a next b))
+
+((a+1)..(a+m))/(n+1)+((a+1)..(a+m))/(n+1)*(n+2)+((a+1)..(a+m))/(n+1)*(n+2)*...(n+m)
+(define (accumulate operation null-value term a next b)
+    (if (> a b)
+        null-value
+        (op (term a) (accumulate operation null-value term (next a) next b))))
+(define (next a) (+ a 1))
+
+(define (f1 a m);chislitel
+(define (term  i) (+ a i));veche sme definirali a v gornata f-q f1 i ne e nujno da q podavame na term
+(accumulate (* 1 term 1 next m)))
+
+(define (f2 n m);znamenatel
+(define (term1 i) (+ n i))
+ (accumulate (* 1 term1 1 next m)))
+ 
+ (define (term a) a)
+(define (func a m n)
+(define (term2 i) (/ (f1 a m) (f2 i m)))
+;(define (next1) (/ (f1 a m) (f2 (+ n 1) m)))
+          (accumulate + 0 term2 1 next n))
