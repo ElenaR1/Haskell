5d528bdb2fac8c67d86a0e048fcd5a9a59da4f18
ElenaR1 <elena.radoycheva@abv.bg>
2017-03-28 23:10:39 +0300

    Create os.sh

---
+++ os.sh
@@ -0,0 +1,4 @@
+#how to find all files with a particular extension
+find . -name "*.txt"
+#find . means, "find (starting in the current directory)." 
+#If you want to search the whole system, use find /; to search under /System/Library, use find /System/Library, etc.
