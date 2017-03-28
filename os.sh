f3d05c0bacfd01af378ad9fc817dc81c14de1451
ElenaR1 <elena.radoycheva@abv.bg>
2017-03-28 23:16:46 +0300

    Update os.sh

--- os.sh
+++ os.sh
@@ -2,3 +2,7 @@
 find . -name "*.txt"
 #find . means, "find (starting in the current directory)." 
 #If you want to search the whole system, use find /; to search under /System/Library, use find /System/Library, etc.
+
+#how to find all files without a particular extension
+find .  ! -name "*.*" 
+#the ! negates the following expression, here a filename that contains a '.'
