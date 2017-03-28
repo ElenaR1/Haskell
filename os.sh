6c3465a28f171ffba8d7311ce7d933142438c6a9
ElenaR1 <elena.radoycheva@abv.bg>
2017-03-28 23:53:24 +0300

    Update os.sh

--- os.sh
+++ os.sh
@@ -6,3 +6,31 @@ find . -name "*.txt"
 #how to find all files without a particular extension
 find .  ! -name "*.*" 
 #the ! negates the following expression, here a filename that contains a '.'
+
+#0zad
+echo LIST/home
+ls /home
+echo
+echo LIST current dir
+ls 
+echo
+cat text.txt
+
+
+#1zad
+ps aux|grep vim|wc -l
+
+#2zad
+find . ! -name "*.txt"|wc -l
+
+#3zad
+sleep 5; ps| wc -l
+
+#4zad
+echo LIST/$1
+ls /$1
+echo
+echo LIST current dir
+ls 
+echo
+cat $2
