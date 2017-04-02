d5b41bc26264ae74b495bf09d570d73f06170819
ElenaR1 <elena.radoycheva@abv.bg>
2017-04-02 14:38:00 +0300

    Update os.sh

--- os.sh
+++ os.sh
@@ -34,3 +34,37 @@ echo LIST current dir
 ls 
 echo
 cat $2
+
+
+
+
+Zaki
+comproc5
+echo LISt $1
+ls $1
+echo $2
+ls $2
+
+comproc6
+set $1 $2
+bash comproc5 $1 $2
+
+comproc7
+sleep $1; ps aux | wc -l
+
+comproc8
+cp $1 temp
+cp $2 $1
+cp temp $2
+rm temp
+
+comproc9
+write $1 < $4
+write $2 < $4
+write $3 < $4
+
+comproc10
+grep $1 < $2
+grep $1 < $3
+grep $1 < $4
+
