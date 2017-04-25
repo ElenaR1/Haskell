e6ead5df9ce47a20b974feb3a3e8f6df7453fd40
ElenaR1 <elena.radoycheva@abv.bg>
2017-04-26 00:34:13 +0300

    Update and rename Commands to Commands.sh

---
+++ Commands.sh
@@ -0,0 +1,126 @@
+touch file-pravi datata na suzdavane na faila dneshniqt den i chas
+
+
+if test -d $1
+then
+	for name in $1/* #?
+	do
+	   size=`wc -c < $name`
+	   if [ -f $name -a $size -gt $2 ]
+	   then  
+		echo $name
+	   fi
+	done
+else
+	echo There is no such directory
+fi 
+
+
+if ! test -d $HOME/$1
+then 
+	mkdir $HOME/$1
+fi
+num=0
+for var in *
+do
+	if  test -f $var -a -r $var 
+	then
+	  cp $var $HOME/$1
+	else 
+	  num=`expr $num + 1 `
+	fi
+done
+echo "Number of nonreadable files = $num"
+
+
+OUT_FILES
+fdir1=`ls $1/*.out | wc -l`
+fdir2=`ls $2/*.out |wc -l`
+if [ $fdri1 -gt $fdir2 ]
+then
+	mkdir NEWdir
+	echo Name of the new directory --> NEWdir
+	tag_dir=$PWD/NEWdir
+	cd $1
+	for i in *
+	do
+	    if [ -f $i -a -r $i -a -w $i ]
+	    then
+		mv $i $tag_dir
+	    fi
+	done
+fi
+
+
+TOT_CI
+fdir1=`ls $1/*.c | wc -l`
+fdir2=`ls $2/*.c | wc -l`
+totbr=`expr $fdir1 + $fdir2`
+if [ $totbr -gt 20 ]
+then
+	ls $1/*.c > /temp/filenames
+	ls $2/*.c >> /temp/filenames
+	chmod 0444 /temp/filenames
+else
+	echo "Total number of files less than 20=$totbr" 
+fi
+
+LOGINFO
+The \c keeps the cursor on the same line after the end of the echo, but to enable it, you need the -e flag:
+echo -e "bla bla \c"
+echo -e "The current date and time :  \c"
+date
+echo -e "Number of users logged --> \c"
+who | wc -l
+
+EXE_FILES #namira broq na vsichkmi failove koito ne sa s razshirenie .c
+ls $1/*.c  > exenames
+broiexe=`cat exenames | wc -w`
+totalcount=`ls $1 | wc -l` #broq na vsichki failove
+echo Count of files `expr $totalcount - $broiexe`
+
+MESS_1 USER
+read key
+write $key << !
+Hello
+!
+
+MESS_USERS
+echo ENTER THE MESSAGE
+read MESSAGE
+for each
+do
+	if who | grep $each > /dev/null
+	then
+	    echo $MESSAGE | write $each
+	fi
+done
+
+if test -f kkk > /dev/null
+then
+echo lala
+else
+echo f
+fi
+
+EXCHAN_NAMES
+mv $1 work
+mv $2 $1
+mv work $2
+
+NUMBER_TERM
+a=`who |wc -l`
+b=`who | grep $1 |wc -l`
+c=`who | grep $2 |wc -l`
+d=`expr $b + $c`
+e=`expr $a - $d`
+echo "Broyiat na terminalite e: $e"
+
+STRING_SEARCH
+echo "ENTER a string --->"
+read STR
+for each #kato ne e set-nat po default obkojda vsichki pozicionni parametri
+ do
+        echo $each
+	grep  $STR $each  
+ done
